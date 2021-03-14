320ca2abe792360dec415d22b4ff547c7c3e6683
barissayil <barissayil@protonmail.com>
2021-03-14 12:30:20 +0100

    Fix

--- src/sdm/algorithms/deep/osdpomdp/agents.cpp
+++ src/sdm/algorithms/deep/osdpomdp/agents.cpp
@@ -12,6 +12,8 @@ namespace sdm{
 		this->agent_1_policy_net = DQN(agent_1_policy_net_input_dim, agent_1_policy_net_inner_dim, agent_1_policy_net_output_dim);
 		this->agent_1_target_net = DQN(agent_1_policy_net_input_dim, agent_1_policy_net_inner_dim, agent_1_policy_net_output_dim);
 		// Put the nets the correct device (CPU/GPU).
+		this->agent_2_transition_net->to(device);
+		this->agent_1_transition_net->to(device);
 		this->agent_1_policy_net->to(device);
 		this->agent_1_target_net->to(device);
 
