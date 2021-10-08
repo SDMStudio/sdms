14bf83e5d7236b835c8abd89f1f3c753aea3202a
davalb <david.albert@insa-rouen.fr>
2021-10-08 10:17:59 +0200

    Temporary comment PWLC QValue

--- src/sdm/utils/value_function/pwlc_qvalue_function.cpp
+++ src/sdm/utils/value_function/pwlc_qvalue_function.cpp
@@ -1,89 +1,89 @@
-#include <sdm/utils/value_function/pwlc_qvalue_function.hpp>
+// #include <sdm/utils/value_function/pwlc_qvalue_function.hpp>
 
-namespace sdm
-{
-    PieceWiseLinearConvexQValueFunction::PieceWiseLinearConvexQValueFunction(number horizon, std::shared_ptr<QInitializer> initializer)
-        : QValueFunction(horizon), initializer_(initializer)
-    {
-        this->representation = std::vector<PSI>(this->isInfiniteHorizon() ? 1 : this->getHorizon() + 1, PSI());
-    }
+// namespace sdm
+// {
+//     PieceWiseLinearConvexQValueFunction::PieceWiseLinearConvexQValueFunction(number horizon, std::shared_ptr<QInitializer> initializer)
+//         : QValueFunction(horizon), initializer_(initializer)
+//     {
+//         this->representation = std::vector<PSI>(this->isInfiniteHorizon() ? 1 : this->getHorizon() + 1, PSI());
+//     }
 
-    PieceWiseLinearConvexQValueFunction::PieceWiseLinearConvexQValueFunction(number horizon, double default_value) : PieceWiseLinearConvexQValueFunction(horizon, std::make_shared<ValueInitializer>(default_value))
-    {
-    }
+//     PieceWiseLinearConvexQValueFunction::PieceWiseLinearConvexQValueFunction(number horizon, double default_value) : PieceWiseLinearConvexQValueFunction(horizon, std::make_shared<ValueInitializer>(default_value))
+//     {
+//     }
 
-    void PieceWiseLinearConvexQValueFunction::initialize()
-    {
-    }
+//     void PieceWiseLinearConvexQValueFunction::initialize()
+//     {
+//     }
 
-    void PieceWiseLinearConvexQValueFunction::initialize(double default_value, number t)
-    {
-    }
+//     void PieceWiseLinearConvexQValueFunction::initialize(double default_value, number t)
+//     {
+//     }
 
-    std::shared_ptr<VectorInterface<std::shared_ptr<Action>, double>> PieceWiseLinearConvexQValueFunction::getQValuesAt(const std::shared_ptr<State> &state, number t)
-    {
-        throw sdm::exception::NotImplementedException();
-    }
+//     std::shared_ptr<VectorInterface<std::shared_ptr<Action>, double>> PieceWiseLinearConvexQValueFunction::getQValuesAt(const std::shared_ptr<State> &state, number t)
+//     {
+//         throw sdm::exception::NotImplementedException();
+//     }
 
-    double PieceWiseLinearConvexQValueFunction::getQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t)
-    {
-        throw sdm::exception::NotImplementedException();
-    }
+//     double PieceWiseLinearConvexQValueFunction::getQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t)
+//     {
+//         throw sdm::exception::NotImplementedException();
+//     }
 
-    double PieceWiseLinearConvexQValueFunction::getQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t)
-    {
-        auto h = this->isInfiniteHorizon() ? 0 : t;
-        return this->representation[h].at(s).getQValueAt(o, u, t);
-    }
+//     double PieceWiseLinearConvexQValueFunction::getQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t)
+//     {
+//         auto h = this->isInfiniteHorizon() ? 0 : t;
+//         return this->representation[h].at(s).getQValueAt(o, u, t);
+//     }
 
-    void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t, double delta)
-    {
-        auto h = this->isInfiniteHorizon() ? 0 : t;
-        for (auto o : state->toOccupancyState()->getJointHistories())
-        {
-            auto u = action->toDecisionRule()->act(o);
-            this->representation[h].at(state->toOccupancyState()).updateQValueAt(o, u, t, delta * state->toOccupancyState()->getProbability(o));
-        }
-    }
+//     void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t, double delta)
+//     {
+//         auto h = this->isInfiniteHorizon() ? 0 : t;
+//         for (auto o : state->toOccupancyState()->getJointHistories())
+//         {
+//             auto u = action->toDecisionRule()->act(o);
+//             this->representation[h].at(state->toOccupancyState()).updateQValueAt(o, u, t, delta * state->toOccupancyState()->getProbability(o));
+//         }
+//     }
 
-    void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t, double value)
-    {
-        throw sdm::exception::NotImplementedException();
-    }
+//     void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t, double value)
+//     {
+//         throw sdm::exception::NotImplementedException();
+//     }
 
-    void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<State> &, const std::shared_ptr<Action> &, number)
-    {
-        throw sdm::exception::NotImplementedException();
-    }
+//     void PieceWiseLinearConvexQValueFunction::updateQValueAt(const std::shared_ptr<State> &, const std::shared_ptr<Action> &, number)
+//     {
+//         throw sdm::exception::NotImplementedException();
+//     }
 
-    int PieceWiseLinearConvexQValueFunction::getNumStates() const
-    {
-        // return this->num_states_;
-        return 0;
-    }
+//     int PieceWiseLinearConvexQValueFunction::getNumStates() const
+//     {
+//         // return this->num_states_;
+//         return 0;
+//     }
 
-    std::string PieceWiseLinearConvexQValueFunction::str() const
-    {
-        std::ostringstream res;
-        // res << "<hierarchical_qvalue_function_v2 horizon=\"" << ((this->isInfiniteHorizon()) ? "inf" : std::to_string(this->getHorizon())) << "\">" << std::endl;
-        // for (sdm::size_t i = 0; i < this->representation.size(); i++)
-        // {
-        //     res << "\t<timestep=\"" << ((this->isInfiniteHorizon()) ? "all" : std::to_string(i)) << "\" default=\"" << 0 << "\">" << std::endl;
-        //     for (auto const& [s, q] : this->representation[i])
-        //     {
-        //         auto jh = *s->getJointHistories().begin();
-        //         if (jh->getHorizon() < this->horizon_)
-        //         {
-        //             res << "\t\t<S-Q>" << std::endl;
-        //             tools::indentedOutput(res, s->str().c_str(), 3);
-        //             res << std::endl;
-        //             tools::indentedOutput(res, q.str().c_str(), 3);
-        //             res << "\t\t</S-Q>" << std::endl;
-        //         }
-        //     }
-        //     res << "\t</timestep>" << std::endl;
-        // }
-        // res << "</hierarchical_qvalue_function_v2>" << std::endl;
-        return res.str();
-    }
-} // namespace sdm
+//     std::string PieceWiseLinearConvexQValueFunction::str() const
+//     {
+//         std::ostringstream res;
+//         // res << "<hierarchical_qvalue_function_v2 horizon=\"" << ((this->isInfiniteHorizon()) ? "inf" : std::to_string(this->getHorizon())) << "\">" << std::endl;
+//         // for (sdm::size_t i = 0; i < this->representation.size(); i++)
+//         // {
+//         //     res << "\t<timestep=\"" << ((this->isInfiniteHorizon()) ? "all" : std::to_string(i)) << "\" default=\"" << 0 << "\">" << std::endl;
+//         //     for (auto const& [s, q] : this->representation[i])
+//         //     {
+//         //         auto jh = *s->getJointHistories().begin();
+//         //         if (jh->getHorizon() < this->horizon_)
+//         //         {
+//         //             res << "\t\t<S-Q>" << std::endl;
+//         //             tools::indentedOutput(res, s->str().c_str(), 3);
+//         //             res << std::endl;
+//         //             tools::indentedOutput(res, q.str().c_str(), 3);
+//         //             res << "\t\t</S-Q>" << std::endl;
+//         //         }
+//         //     }
+//         //     res << "\t</timestep>" << std::endl;
+//         // }
+//         // res << "</hierarchical_qvalue_function_v2>" << std::endl;
+//         return res.str();
+//     }
+// } // namespace sdm
--- src/sdm/utils/value_function/pwlc_qvalue_function.hpp
+++ src/sdm/utils/value_function/pwlc_qvalue_function.hpp
@@ -1,93 +1,93 @@
-#pragma once
+// #pragma once
 
-#include <sdm/utils/value_function/tabular_qvalue_function.hpp>
-#include <sdm/core/state/interface/occupancy_state_interface.hpp>
-/**
- * @brief Namespace grouping all tools required for sequential decision making.
- * @namespace  sdm
- */
-namespace sdm
-{
-    class PieceWiseLinearConvexQValueFunction : public QValueFunction
-    {
-    protected:
-        using PSI = std::unordered_map<std::shared_ptr<OccupancyStateInterface>, TabularQValueFunction /*, HashSpecific, EqualSpecific*/>;
+// #include <sdm/utils/value_function/tabular_qvalue_function.hpp>
+// #include <sdm/core/state/interface/occupancy_state_interface.hpp>
+// /**
+//  * @brief Namespace grouping all tools required for sequential decision making.
+//  * @namespace  sdm
+//  */
+// namespace sdm
+// {
+//     class PieceWiseLinearConvexQValueFunction : public QValueFunction
+//     {
+//     protected:
+//         using PSI = std::unordered_map<std::shared_ptr<OccupancyStateInterface>, TabularQValueFunction /*, HashSpecific, EqualSpecific*/>;
 
-        /**
-         * @brief The precision used to assign a representant to an occupancy states.
-         */
-        double granularity = 0.75;
+//         /**
+//          * @brief The precision used to assign a representant to an occupancy states.
+//          */
+//         double granularity = 0.75;
 
-        /**
-         * @brief The datastructure storing the value function representation. 
-         * 
-         */
-        std::vector<PSI> representation;
+//         /**
+//          * @brief The datastructure storing the value function representation. 
+//          * 
+//          */
+//         std::vector<PSI> representation;
 
-        /**
-         * @brief The initializer to use for this value function. 
-         * 
-         */
-        std::shared_ptr<QInitializer> initializer_;
+//         /**
+//          * @brief The initializer to use for this value function. 
+//          * 
+//          */
+//         std::shared_ptr<QInitializer> initializer_;
 
-    public:
+//     public:
 
-        PieceWiseLinearConvexQValueFunction(number horizon, std::shared_ptr<QInitializer> initializer);
-        PieceWiseLinearConvexQValueFunction(number horizon = 0, double default_value = 0.);
+//         PieceWiseLinearConvexQValueFunction(number horizon, std::shared_ptr<QInitializer> initializer);
+//         PieceWiseLinearConvexQValueFunction(number horizon = 0, double default_value = 0.);
 
-        /**
-         * @brief Initialize the value function 
-         */
-        void initialize();
+//         /**
+//          * @brief Initialize the value function 
+//          */
+//         void initialize();
 
-        /**
-         * @brief Initialize the value function with a default value
-         */
-        void initialize(double v, number t = 0);
+//         /**
+//          * @brief Initialize the value function with a default value
+//          */
+//         void initialize(double v, number t = 0);
 
-        /**
-         * @brief Get the q-value at a state
-         * 
-         * @param state the state
-         * @return the action value vector 
-         */
-        std::shared_ptr<VectorInterface<std::shared_ptr<Action>, double>> getQValuesAt(const std::shared_ptr<State> &state, number t);
+//         /**
+//          * @brief Get the q-value at a state
+//          * 
+//          * @param state the state
+//          * @return the action value vector 
+//          */
+//         std::shared_ptr<VectorInterface<std::shared_ptr<Action>, double>> getQValuesAt(const std::shared_ptr<State> &state, number t);
 
-        /**
-         * @brief Get the q-value given state and action
-         * 
-         * @param state the state
-         * @param action the action
-         * @return the q-value
-         */
-        double getQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t);
+//         /**
+//          * @brief Get the q-value given state and action
+//          * 
+//          * @param state the state
+//          * @param action the action
+//          * @return the q-value
+//          */
+//         double getQValueAt(const std::shared_ptr<OccupancyStateInterface> &s, const std::shared_ptr<JointHistoryInterface> &o, const std::shared_ptr<Action> &u, number t);
 
-        double getQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t);
+//         double getQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t);
 
-        /**
-         * @brief Update the value at a given state
-         */
-        void updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t = 0);
+//         /**
+//          * @brief Update the value at a given state
+//          */
+//         void updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t = 0);
 
-        /**
-         * @brief Update the value at a given state (given a delta)
-         */
-        void updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t, double delta);
+//         /**
+//          * @brief Update the value at a given state (given a delta)
+//          */
+//         void updateQValueAt(const std::shared_ptr<State> &state, const std::shared_ptr<Action> &action, number t, double delta);
 
-        void updateQValueAt(const std::shared_ptr<OccupancyStateInterface> &state, const std::shared_ptr<JointHistoryInterface> &history, const std::shared_ptr<Action> &action, number t, double delta);
+//         void updateQValueAt(const std::shared_ptr<OccupancyStateInterface> &state, const std::shared_ptr<JointHistoryInterface> &history, const std::shared_ptr<Action> &action, number t, double delta);
 
-        int getNumStates() const;
+//         int getNumStates() const;
 
-        /**
-         * @brief Define this function in order to be able to display the value function
-         */
-        virtual std::string str() const;
+//         /**
+//          * @brief Define this function in order to be able to display the value function
+//          */
+//         virtual std::string str() const;
 
-        friend std::ostream &operator<<(std::ostream &os, PieceWiseLinearConvexQValueFunction &vf)
-        {
-            os << vf.str();
-            return os;
-        }
-    };
+//         friend std::ostream &operator<<(std::ostream &os, PieceWiseLinearConvexQValueFunction &vf)
+//         {
+//             os << vf.str();
+//             return os;
+//         }
+//     };
 
-} // namespace sdm
+// } // namespace sdm
