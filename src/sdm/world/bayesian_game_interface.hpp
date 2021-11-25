2e3ff297f03a980cd2e70cb4c14b84bbfc889cc8
davalb <david.albert@insa-rouen.fr>
2021-11-25 12:19:46 +0100

    DA : Expected interface

--- src/sdm/world/bayesian_game_interface.hpp
+++ src/sdm/world/bayesian_game_interface.hpp
@@ -4,24 +4,48 @@
 #include <sdm/types.hpp>
 #include <sdm/core/state/state.hpp>
 #include <sdm/core/action/action.hpp>
+#include <sdm/core/space/space.hpp>
 
-namespace sdm {
+namespace sdm
+{
 
-    class BayesianGameInterface {
-        
-        public:
+    class BayesianGameInterface
+    {
 
-            virtual ~BayesianGameInterface(){}
+    public:
+        virtual ~BayesianGameInterface() {}
 
-            virtual number getNumAgents() const = 0;
+        virtual number getNumAgents() const = 0;
 
-            virtual std::vector<int> getGameDimensions() = 0;
+        virtual std::vector<int> getGameDimensions() = 0;
 
-            virtual std::vector<int> getTypesNumbers() = 0;
+        virtual std::vector<int> getTypesNumbers() = 0;
 
-            virtual float getPayoff(std::vector<std::shared_ptr<State>> types, std::vector<std::shared_ptr<Action>> actions, int idAgent) = 0;
+        virtual float getPayoff(std::vector<std::shared_ptr<State>> types, std::vector<std::shared_ptr<Action>> actions, int idAgent) = 0;
 
-            virtual float getJointTypesProba(std::vector<std::shared_ptr<State>> types) = 0;
+        virtual float getJointTypesProba(std::vector<std::shared_ptr<State>> types) = 0;
+    };
 
+    class BayesianGameInterfaceTMP
+    {
+    public:
+        virtual ~BayesianGameInterfaceTMP() {}
+        virtual number getNumAgents() const = 0;
+
+        // Dans TwoPlayerBayesianGame la structure de donnee pour cela est un MultiDiscreteSpace
+        // composé de N espaces d'actions individuels (un pour chaque joueur)
+        virtual std::shared_ptr<Space> getActionSpace() const = 0;
+
+        // Dans TwoPlayerBayesianGame la structure de donnee pour cela est un MultiDiscreteSpace
+        // composé de N espaces de types individuels (un pour chaque joueur)
+        virtual std::shared_ptr<Space> getTypeSpace() const = 0;
+
+        // Les MultiDiscreteSpace ci-dessus te génèrent des std:shared_ptr<Joint<std::shared_ptr<State>>>,
+        // C'est grossomodo la même chose que des pointeurs sur std::vector<std::shared_ptr<State>> mais qui sont reconnus
+        // comme des std::shared<State> (Joint<std::shared_ptr<State>> hérite de State).
+        // C'est ces derniers qui seront passés en argument de tes fonctions
+        virtual double getPayoff(std::shared_ptr<State> joint_type, std::shared_ptr<Action> joint_action, int idAgent) = 0;
+
+        virtual double getJointTypesProba(std::shared_ptr<State> joint_type) = 0;
     };
 }
