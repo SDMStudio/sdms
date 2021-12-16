488c4ea5995b1d3bc1644e0cd6ff2a886100c8ee
davalb <david.albert@insa-rouen.fr>
2021-12-16 10:17:29 +0100

    Update test file

--- tests/test_hyperparams.sh
+++ tests/test_hyperparams.sh
@@ -11,7 +11,7 @@ do
     for (( h=1; h<=$max_horizon; h++ ))
     do 
         echo "Run HSVI, ${worlds[$i]}, h=$h, mem=${memory[$i]}, please wait ..."
-        SDMStudio solve -a HSVI -f oMDP -h $h -m ${memory[$i]} -e 0.01 --p_c 0.1 --p_o 0.01 --p_b 0.001 --lower_bound maxplan_wcsp --lb_freq_pruning 10 --lb_type_of_pruning bounded --upper_bound sawtooth_lp --ub_init Pomdp --time_max 7200 -w "${worlds[$i]}.dpomdp" --name "hsvi_${worlds[$i]}_${h}_${memory}_wcsp_lp"
+        SDMStudio solve -a HSVI -f oMDP -h $h -m ${memory[$i]} -e 0.01 --p_c 0.1 --p_o 0.01 --p_b 0.001 --lower_bound maxplan_wcsp --lb_freq_pruning 10 --lb_type_of_pruning pairwise --upper_bound sawtooth_lp --ub_init Pomdp --ub_freq_pruning 10 --ub_type_of_pruning pairwise --time_max 7200 -w "${worlds[$i]}.dpomdp" --name "hsvi_${worlds[$i]}_${h}_wcsp_lp"
         # echo "Run sequential HSVI, world=$world, h=$h, please wait ..."
         # SDMStudio solve -a HSVI -f ext-oMDP -h $h -m ${memory[$i]} -e 0.01 --p_c 0.1 --p_o 0.01 --p_b 0.001 --lower_bound maxplan --lb_freq_pruning 10 --lb_type_of_pruning bounded --upper_bound sawtooth_lp --ub_init Pomdp --time_max 7200 -w "${world}.dpomdp" --name "ext-hsvi_${world}_${h}_${memory}_wcsp_lp"
         # echo "Run PBVI, world=$world, h=$h, please wait ..."
